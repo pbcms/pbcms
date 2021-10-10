dbc216f5a211e15e4c0de883dc44937bac8ac9db
Micha de Vries <contact@michadevries.nl>
2021-10-10 11:56:05 +0200

    Created empty APIs

---
+++ app/api/User.php
@@ -0,0 +1,55 @@
+<?php
+    use Library\User;
+    use Library\UserPermissions;
+
+    $this->__registerMethod('create', function() {
+
+    });
+
+    $this->__registerMethod('find', function() {
+
+    });
+
+    $this->__registerMethod('info', function() {
+
+    });
+
+    $this->__registerMethod('list', function() {
+
+    });
+
+    $this->__registerMethod('update', function() {
+
+    });
+
+    $this->__registerMethod('delete', function() {
+
+    });
+
+    $this->__registerMethod('get-id', function() {
+
+    });
+
+    $this->__registerMethod('meta-get', function() {
+
+    });
+
+    $this->__registerMethod('meta-list', function() {
+
+    });
+
+    $this->__registerMethod('meta-exists', function() {
+
+    });
+
+    $this->__registerMethod('meta-set', function() {
+
+    });
+
+    $this->__registerMethod('meta-delete', function() {
+
+    });
+
+    $this->__registerMethod('purge-meta', function() {
+
+    });
