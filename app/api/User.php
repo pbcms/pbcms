9da34448fcc4884609281aed9a0ab12258755fd5
Micha de Vries <contact@michadevries.nl>
2021-11-22 10:28:15 +0100

    Removed find API, same as info API.

--- app/api/User.php
+++ app/api/User.php
@@ -23,36 +23,8 @@
         }
     });
 
-    $this->__registerMethod('find', function() {
-        $required = array("identifier");
-        $allowed = array("identifier", "by_id_allowed");
-        if (!Request::requireMethod('post')) die();
-        if (!Request::requireAuthentication()) die();
-
-        if ($this->user->check('user.find')) {
-            $postdata = Request::parsePost();
-            $options = Validator::trimObject($postdata);
-            $missing = Validator::listMissing($required, $options);
-            if (count($missing) == 0) {
-                $options = (object) Validator::removeUnlisted($allowed, $options);
-                $users = new Users;
-                $result = $users->find($options->identifier, (isset($options->by_id_allowed) ? (intval($options->by_id_allowed) == 1 ? true : false) : true));
-                Respond::success((object) array(
-                    "user" => $result
-                ));
-            } else {
-                Respond::error("missing_information", array(
-                    "message" => 'The following post information is missing from the request: ' . join(',', $missing) . '.',
-                    "missing_info" => $missing
-                ));
-            }
-        } else {
-            Respond::error("missing_privileges", $this->lang->get("messages.api-user.error_missing_privileges", "You are lacking the permission to create a new user."));
-        }
-    });
-
     $this->__registerMethod('info', function() {
-
+        
     });
 
     $this->__registerMethod('list', function() {
