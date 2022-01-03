f58805b6694686ba1cdb67ddf181cf13643e5a96
Micha de Vries <contact@michadevries.nl>
2022-01-03 12:54:31 +0100

    removed debugging var_dump

--- app/sources/migrations/0.0.12-2-add_session_properties_column.php
+++ app/sources/migrations/0.0.12-2-add_session_properties_column.php
@@ -4,7 +4,6 @@
     class AddSessionPropertiesColumn__2__0_0_12 {
         public function up($db, $log) {
             $res = $db->query("ALTER TABLE `" . DATABASE_TABLE_PREFIX . "sessions` ADD `properties` JSON NOT NULL DEFAULT ('[]') AFTER `uuid`");
-            var_dump($res);
         }
 
         public function down($up, $log) {
