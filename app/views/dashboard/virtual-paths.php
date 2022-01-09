c909d10d928ffe3aa698c6d5267c960de979fbd5
Micha de Vries <contact@michadevries.nl>
2022-01-09 15:26:10 +0100

    Created initial view for virtual paths.

---
+++ app/views/dashboard/virtual-paths.php
@@ -0,0 +1,49 @@
+<?php
+    use Library\VirtualPath;
+
+    $vPaths = new VirtualPath;
+    $vPathList = $vPaths->list();
+    $tableContent = '';
+
+    foreach($vPathList as $vPath) {
+        $tableContent .= renderRow((object) $vPath);
+    }
+
+    function renderRow($item) {
+        $result = '<tr id="' . $item->id. '"><td>' . $item->id . '</td><td>' . $item->path . '</td><td>' . $item->target . '</td><td>' . $item->lang . '</td></tr>';
+        return $result;
+    }
+?>
+
+<section class="page-introduction">
+    <h1>
+        Virtual paths
+    </h1>
+    <p>
+        View virtual paths on your website.
+    </p>
+</section>
+
+<section class="no-padding transparent">
+    <table>
+        <thead>
+            <th>
+                ID
+            </th>
+            <th>
+                Path
+            </th>
+            <th>
+                Target
+            </th>
+            <th>
+                Language
+            </th>
+        </thead>
+        <tbody>
+            <?php
+                echo $tableContent;
+            ?>
+        </tbody>
+    </table>
+</section>
