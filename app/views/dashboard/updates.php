72d29e506cc300a7e4989a457ce6e1cfdcab1016
Micha de Vries <contact@michadevries.nl>
2022-01-27 13:22:05 +0100

    Started on design for update page.

---
+++ app/views/dashboard/updates.php
@@ -0,0 +1,18 @@
+<section class="page-introduction columns">
+    <div>
+        <h1>
+            Updates
+        </h1>
+        <p>
+            Keep your installation and it's modules up-to-date.
+        </p>
+    </div>
+    <div class="icon-button">
+        <i data-feather="settings"></i>
+    </div>
+</section>
+<section>
+    <h2>
+        Modules
+    </h2>
+</section>
