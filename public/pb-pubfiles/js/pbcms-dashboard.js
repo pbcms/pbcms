9deebc7ffe3ebb1d42687549197205a4a74d8dce
Micha de Vries <contact@michadevries.nl>
2022-02-02 16:35:18 +0100

    Dashboard backup section.

--- public/pb-pubfiles/js/pbcms-dashboard.js
+++ public/pb-pubfiles/js/pbcms-dashboard.js
@@ -2,6 +2,15 @@
 (function() {
     let sideOpts = document.querySelector('.sidebar .sidebar-options');
     let activeSect = sideOpts.querySelector('a[active]');
-    sideOpts.scrollTop = activeSect.offsetTop - sideOpts.offsetTop - (sideOpts.clientHeight / 2) + (activeSect.clientHeight / 2);
-    sideOpts.style.scrollBehavior = 'smooth';
+    let backupSect = sideOpts.querySelector('a[backup-active]');
+
+    if (!activeSect && backupSect) {
+        activeSect = backupSect;
+        backupSect.setAttribute('active', '');
+    }
+
+    if (activeSect) {
+        sideOpts.scrollTop = activeSect.offsetTop - sideOpts.offsetTop - (sideOpts.clientHeight / 2) + (activeSect.clientHeight / 2);
+        sideOpts.style.scrollBehavior = 'smooth';
+    }
 })();
