9d0042ba45a08fbbb2f7a20abf123fabfe20d5e5
Micha de Vries <contact@michadevries.nl>
2022-03-21 13:18:13 +0100

    Change signin url.

--- app/views/pages/error-welcome-page.php
+++ app/views/pages/error-welcome-page.php
@@ -13,7 +13,7 @@
 ?>
 
 <p>
-    <?php echo str_replace("{{SITE_LOCATION}}", SITE_LOCATION, $lang->get('error-pages.error-welcome-page.content-1', "Welcome to your new PCBMS instance, this is a small introduction page. <br>Start by <a href='{{SITE_LOCATION}}pb-auth/signin'>signin in to your account</a> and by exploring the dashboard!")); ?>
+    <?php echo str_replace("{{SITE_LOCATION}}", SITE_LOCATION, $lang->get('error-pages.error-welcome-page.content-1', "Welcome to your new PCBMS instance, this is a small introduction page. <br>Start by <a href='{{SITE_LOCATION}}pb-auth'>signin in to your account</a> and by exploring the dashboard!")); ?>
 </p>
 
 <i>
