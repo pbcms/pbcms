21dde543baa96e90564595eec40096e6563caa86
Micha de Vries <contact@michadevries.nl>
2022-03-29 11:52:45 +0200

    Permission management in Dashboard.

--- app/views/dashboard/permissions.php
+++ app/views/dashboard/permissions.php
@@ -7,6 +7,26 @@
     </p>
 </section>
 
+<section id="new-permission">
+    <h2>
+        New permission
+    </h2>
+
+    <br>
+    <input-field &new_permission_node="value" $placeholder="Permission"></input-field>
+    <input-field &new_permission_target="value" $placeholder="Target"></input-field>
+    <input-toggle &new_permission_granted="checked" $label="Granted"></input-toggle>
+
+    <div class="submitter">
+        <button @click="createPermission()">
+            Create
+        </button>
+        <p class="message" :class:show="showMessage">
+            {{ message }}
+        </p>
+    </div>
+</section>
+
 <section class="no-margin transparent overflow-scroll">
     <table>
         <thead>
@@ -22,15 +42,18 @@
             <th>
                 Type
             </th>
-            <th>
+            <th class="medium">
                 Created
             </th>
-            <th>
+            <th class="medium">
                 Updated
+            </th>
+            <th class="medium">
+                Actions
             </th>
         </thead>
         <tbody>
-            <tr :for="perm in permissions">
+            <tr :for="permissions as index => perm">
                 <td>
                     {{perm.id}}
                 </td>
@@ -48,6 +71,15 @@
                 </td>
                 <td>
                     {{perm.updated}}
+                </td>
+                <td class="multiple-actions">
+                    <a href="@" class="red" @click="clearPermission(perm.id, event)">
+                        clear
+                    </a>
+                    <span>-</span>
+                    <a href="#" @click="perm.type == 'Granted' ? rejectPermission(perm.target, perm.permission, event) : grantPermission(perm.target, perm.permission, event)">
+                        {{ perm.type == 'Granted' ? 'reject' : 'grant' }}
+                    </a>
                 </td>
             </tr>
         </tbody>
--- public/pb-pubfiles/js/pb-pages-dashboard-permissions.js
+++ public/pb-pubfiles/js/pb-pages-dashboard-permissions.js
@@ -1,23 +1,104 @@
 import { Rable } from '../../pb-pubfiles/js/rable.js';
 
+const api = PbAuth.apiInstance();
 const app = new Rable({
     data: {
-        permissions: []
+        permissions: [],
+
+        new_permission_node: "",
+        new_permission_target: "",
+        new_permission_granted: true,
+
+        async refreshPermissions() {
+            const res = await api.post('permissions/list');
+            if (res.data.success == undefined) {
+                alert('An unknown error has occured! (unknown_error)');
+            } else if (res.data.success == false) {
+                alert(res.data.message + ' (' + res.data.error + ')');
+            } else {
+                for(var i = 0; i < res.data.list.length; i++) {
+                    res.data.list[i].type = res.data.list[i].granted == '1' ? "Granted" : "Rejected";
+                }
+            
+                app.data.permissions = res.data.list;
+            }
+        },
+
+        async grantPermission(target, permission, e) {
+            if (e) e.preventDefault();
+            return new Promise(resolve => {
+                api.post('permissions/grant', {
+                    target_type: target.split(':')[0],
+                    target_value: target.split(':')[1],
+                    permission: permission
+                }).then(async res => {
+                    if (res.data && res.data.success) {
+                        await this.refreshPermissions();
+                        resolve(true);
+                    } else {
+                        alert(`${res.data.message} (${res.data.error})`);
+                        resolve(false);
+                    }
+                });
+            });
+        },
+
+        rejectPermission(target, permission, e) {
+            e.preventDefault();
+            return new Promise(resolve => {
+                api.post('permissions/reject', {
+                    target_type: target.split(':')[0],
+                    target_value: target.split(':')[1],
+                    permission: permission
+                }).then(async res => {
+                    if (res.data && res.data.success) {
+                        await this.refreshPermissions();
+                        resolve(true);
+                    } else {
+                        alert(`${res.data.message} (${res.data.error})`);
+                        resolve(false);
+                    }
+                });
+            });
+        },
+
+        async clearPermission(id, e) {
+            e.preventDefault();
+            return new Promise(resolve => {
+                api.delete('permissions/clear', {
+                    data: {
+                        permission_id: id
+                    }
+                }).then(async res => {
+                    if (res.data && res.data.success) {
+                        await this.refreshPermissions();
+                        resolve(true);
+                    } else {
+                        alert(`${res.data.message} (${res.data.error})`);
+                        resolve(false);
+                    }
+                });
+            });
+        },
+
+        async createPermission() {
+            if (this.new_permission_granted) {
+                var success = await this.grantPermission(this.new_permission_target, this.new_permission_node);
+            } else {
+                var success = await this.rejectPermission(this.new_permission_target, this.new_permission_node);
+            }
+
+            if (success) {
+                this.new_permission_node = "";
+                this.new_permission_target = "";
+                this.new_permission_granted = true;
+            }
+        }
     }
 });
 
+await app.importComponent('input-field', SITE_LOCATION + "pb-pubfiles/components/InputField.html");
+await app.importComponent('input-toggle', SITE_LOCATION + "pb-pubfiles/components/InputToggle.html");
 app.mount('.content');
 
-PbAuth.apiInstance().post('permissions/list').then(res => {
-    if (res.data.success == undefined) {
-        alert('An unknown error has occured! (unknown_error)');
-    } else if (res.data.success == false) {
-        alert(res.data.message + ' (' + res.data.error + ')');
-    } else {
-        for(var i = 0; i < res.data.list.length; i++) {
-            res.data.list[i].type = res.data.list[i].granted == '1' ? "Granted" : "Rejected";
-        }
-    
-        app.data.permissions = res.data.list;
-    }
-});
+app.data.refreshPermissions();
